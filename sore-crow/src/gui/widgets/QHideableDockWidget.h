9bfae256fad934ee90c31a4ad98c718f298a8dbe
Alpaca <paulocastrogalvao@hotmail.com>
2023-09-01 16:13:49 -0400

    Adds hideable dock widget

---
+++ sore-crow/src/gui/widgets/QHideableDockWidget.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <QDockWidget>
+
+namespace sore
+{
+	class QHideableDockWidget : public QDockWidget
+	{
+		Q_OBJECT
+
+	public:
+		QHideableDockWidget(QWidget* parent = nullptr)
+			: QDockWidget(parent)
+		{
+
+		}
+
+	protected:
+		void closeEvent(QCloseEvent* event)
+		{
+			hide();
+		}
+	};
+}
