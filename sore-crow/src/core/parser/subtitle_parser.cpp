d4bbc76496226cd09a10bbb3b0f84a542d7eadcb
Alpaca <paulocastrogalvao@hotmail.com>
2023-09-05 23:35:50 -0400

    Minor fixes

--- sore-crow/src/core/parser/subtitle_parser.cpp
+++ sore-crow/src/core/parser/subtitle_parser.cpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "pch.h"
 #include <string>
 #include <fstream>
 
@@ -16,7 +17,7 @@ namespace sore
 	std::vector<SubtitleEntry> SubtitleParser::parseSubtitleFile(const std::string& filepath)
 	{
 		if (!isFileSupportedSubtitle(filepath))
-			return;
+			return {};
 
 		std::ifstream file(filepath);
 
